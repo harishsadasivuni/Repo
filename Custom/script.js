107bb6e105db1fa5da6671e2928db90c5bf22eff
harishsadasivuni <harishsadasivuni@gmail.com>
2022-05-08 20:20:35 +0530

    images update

--- Custom/script.js
+++ Custom/script.js
@@ -18,35 +18,42 @@ var countofcorrect = 0;
   };
 })();
 function setmul(){
-  var first = Math.floor(Math.random() * 16);
+  var first = Math.floor(Math.random() * 18);
   var second = Math.floor(Math.random() * 10);
   var finalanswer = first * second;
   var sendnumber = document.getElementById("question");
-  var sendstring = "How much is "+first+" X "+second+" ?";
+  var sendstring = "How much is "+first+" x "+second+" ?";
   sendnumber.innerHTML = sendstring;
   return finalanswer;
 }
 function correctlist(){
   var val = Math.floor(Math.random() * 4);
   console.log("correct val is "+val);
+  var imagesetter = "";
   switch (val) {
     case 0:
       newmsg = "Nice Yasaswini!";
+      imagesetter = "success1.png";
       break;
     case 2:
       newmsg = "Good Yasaswini!";
+      imagesetter = "success2.webp";
       break;
     case 3:
       newmsg = "Yasaswini is a good girl!";
+      imagesetter = "success3.png";
       break;
     case 1:
       newmsg = "Yasaswini is good at tables!";
+      imagesetter = "success4.png";
       break;
   }
   var sentmsg = document.getElementById("msg");
   var givenanswer = document.getElementById("answer");
+  var imager = document.getElementById("imager");
   givenanswer.style.borderColor = "green";
   givenanswer.style.backgroundColor = "#b6ffc3";
+  imager.setAttribute("src", imagesetter);
   sentmsg.innerHTML = newmsg;
   var countercorrect = document.getElementById("cro");
   countofcorrect++;
@@ -55,23 +62,30 @@ function correctlist(){
 }
 function wronglist(){
   var val = Math.floor(Math.random() * 4);
+  var imagesetter = "";
   console.log("wrong val is "+val);
   switch (val) {
     case 1:
       newmsg = "Yasaswini is a bad girl!";
+      imagesetter = "failure1.png";
       break;
     case 2:
       newmsg = "Yasaswini Don't Know Tables!";
+      imagesetter = "failure2.png";
       break;
     case 3:
       newmsg = "Wrong Yasaswini!";
+      imagesetter = "failure3.gif";
       break;
     case 0:
       newmsg = "Yasaswini...Learn Tables!";
+      imagesetter = "failure4.gif";
       break;
   }
   var sentmsg = document.getElementById("msg");
+  var imager = document.getElementById("imager");
   sentmsg.innerHTML = newmsg;
+  imager.setAttribute("src", imagesetter);
   var givenanswer = document.getElementById("answer");
   givenanswer.style.borderColor = "red";
   givenanswer.style.backgroundColor = "#ffe0e0";
